d4ec42ed82a43f61c2c77a6836185bf8044771dc
chapuzzo <luismax@gmail.com>
2017-04-21 20:21:40 +0200

    indentation

--- Service/NotificationGenerator.php
+++ Service/NotificationGenerator.php
@@ -12,33 +12,31 @@ class NotificationGenerator
 
 	public function __construct($api_key, $app_id, $user_auth_key)
 	{
-	  $this->api_key = $api_key;
-	  $this->app_id = $app_id;
-	  $this->user_auth_key = $user_auth_key;
+		$this->api_key = $api_key;
+		$this->app_id = $app_id;
+		$this->user_auth_key = $user_auth_key;
 	}
 
 	public function generate($text)
 	{
-	  $oneSignal = new OneSignal($this->user_auth_key, $this->app_id, $this->api_key);
-	  $notification = new Notification($oneSignal);
+		$oneSignal = new OneSignal($this->user_auth_key, $this->app_id, $this->api_key);
+		$notification = new Notification($oneSignal);
 
-	  $notificationData = [
-	      'included_segments' => ['All'],
-	      'contents' => [
-	          'en' => 'Hello, world',
-	      ],
-	      'headings' => [
-	          'en' => $text,
-	      ],
-	      'buttons' => [
-	          [
-	              'id' => 'button_id',
-	              'text' => 'Button text',
-	              'icon' => 'button_icon',
-	          ],
-	      ]
-	  ];
+		$notificationData = [
+			'included_segments' => ['All'],
+			'contents' => [
+					'en' => 'Hello, world',
+			],
+			'data' => [
+				'url' => 'http://todler.es'
+			],
+			'headings' => [
+					'en' => $text,
+			],
+			'android_group' => 'bullshit',
+			'collapse_id' => 'bullshit'
+		];
 
-	  $notification->create($notificationData);
+		$notification->create($notificationData);
 	}
 }
